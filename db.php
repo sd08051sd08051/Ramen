6e53407c7099175a846436b3bb62296c81af6c3d
sd08051sd08051 <southafrica.2018.durban@gmail.com>
2024-02-18 16:54:03 +0900

    22

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+// $host = "mysql57.southafrica2018.sakura.ne.jp";
+// $dbName = "southafrica2018_ramen";
+// $user = "southafrica2018";
+// $password = "CyEt4x8n";
+// $dsn = "mysql:host={$host};dbname={$dbName};charser=utf8";
+
+$production_server_env = (object) [
+    'db_name' => 'southafrica2018_ramen',
+    'table_name_in_DB' => ['comments'],
+    'charset'=> "utf8",
+    'host' => "mysql57.southafrica2018.sakura.ne.jp",
+    'id' => "southafrica2018",
+    'pw' => "",
+];
+
+?>
